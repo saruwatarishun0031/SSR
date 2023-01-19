ebd6d07d0518772511a7bc5d9596b1e397b2c06b
shun_a123 <sshuna123@gmail.com>
2023-01-19 15:13:47 +0900

    [Add] Sato2に生まれ変わりました　弾を吹き出す障害物追加

--- Assets/Sato/Script/DeadSystem.cs
+++ Assets/Sato/Script/DeadSystem.cs
@@ -4,16 +4,21 @@ using UnityEngine;
 
 public class DeadSystem : MonoBehaviour
 {
-    [SerializeField] private float moveSpeed = 5f;
+    Rigidbody2D _rb;
 
-    void Update()
+    [SerializeField] float _moveSpeed = 1.0f;
+
+    private void Awake()
     {
-        transform.Translate(transform.localScale * moveSpeed * Time.deltaTime);
+        _rb = gameObject.GetComponent<Rigidbody2D>();
     }
-
+    private void Start()
+    {
+        _rb.AddForce(Vector2.up * _moveSpeed);
+    }
     private void OnCollisionEnter(Collision collision)
     {
-        if(collision.gameObject.tag == "Player")
+        if (collision.gameObject.tag == "Player")
         {
             Destroy(collision.gameObject);
         }
