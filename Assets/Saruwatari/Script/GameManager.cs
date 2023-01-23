a337fc7d22113ae5f37dce471647227491f262f8
SHUNSHUN-0031 <saruwatarishun15@gmail.com>
2023-01-23 18:11:52 +0900

    [Add]GameManajer追加

---
+++ Assets/Saruwatari/Script/GameManager.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    MoveObject2D object2D;
+
+    [SerializeField] Canvas _result;
+
+    //シングルトンパターン（簡易型、呼び出される）
+    public static GameManager Instance;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(this);
+        }
+    }
+    //シングルトン（ここまで）
+    // Start is called before the first frame update
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        object2D = GetComponent<MoveObject2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GameOver();
+        Clear();
+    }
+
+    void GameOver()
+    {
+       if (object2D._gameover == true)
+       {
+            _result.gameObject.SetActive(true);
+       }
+    }
+
+    void Clear()
+    {
+        if (object2D._clear == true)
+        {
+            SceneManager.LoadScene("Result");
+        }
+    }
+}
--- Assets/Saruwatari/Script/MoveObject2D.cs
+++ Assets/Saruwatari/Script/MoveObject2D.cs
@@ -4,15 +4,39 @@ using UnityEngine;
 
 public class MoveObject2D : MonoBehaviour
 {
+    Rigidbody2D _rb;
+
+    [SerializeField, Range(1, 100), Header("‘¬‚³")]
+    private float Speed;
+
+    public bool _gameover;
+    public bool _clear;
     // Start is called before the first frame update
     void Start()
     {
-        
+        _rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Move();
+    }
+
+    void Move()
+    {
+        _rb.velocity = _rb.transform.right * Speed;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "" || collision.gameObject.name == "")
+        {
+            _gameover = true;
+        }
+        else if (collision.gameObject.name == "")
+        {
+            _clear = true;
+        }
     }
 }
