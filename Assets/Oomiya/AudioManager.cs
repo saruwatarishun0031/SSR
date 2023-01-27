287e1127d341e868c5746ad06d83620fad9b5163
ryuoomiya <oomiyaryu@gmail.com>
2023-01-27 13:17:30 +0900

    [Add] AudioManagerにゲームクリア時のメソッドを追加

--- Assets/Oomiya/AudioManager.cs
+++ Assets/Oomiya/AudioManager.cs
@@ -2,18 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-//
 public class AudioManager : MonoBehaviour
 {
     [SerializeField] AudioSource _as;
-    void Start()
-    {
-        
-    }
-
+    [SerializeField] AudioClip _clearSE;
+ 
     //ボタン押したとき用のメソッド
     public void OnClickButton(AudioClip ac)
     {
         _as.PlayOneShot(ac);
+    }
+
+    //ゴールについたら呼ぶ
+    public void GameClear()
+    {
+        _as.PlayOneShot(_clearSE);
     }
 }
